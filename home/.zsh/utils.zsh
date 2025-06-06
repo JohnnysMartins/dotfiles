c543c4071f00c5fed635c3c7aeb06c7be3b4eadd
Johnnys Martins <johnnys.adson@gmail.com>
2025-06-06 10:30:59 -0300

    add list ips function

--- home/.zsh/utils.zsh
+++ home/.zsh/utils.zsh
@@ -31,3 +31,18 @@ function source_if_exists() {
     source "$file" &> /dev/null
   fi
 }
+
+function list_ips() {
+  ip -4 addr show scope global | awk '
+    /^[0-9]+:/ {
+      iface = $2
+      sub(/:$/, "", iface)
+    }
+    /inet / {
+      ip = $2
+      sub(/\/.*/, "", ip)
+      print iface ": " ip
+    }
+  '
+}
+
